a793184be0adc1fe8dc3cbb055d94a73210abb30
waynenorris1 <wayneamazan0@gmail.com>
2024-04-26 23:16:48 +0800

    display login page and migrated accounts table

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/loginpage', function () {
+    return view('login');
+}); 
+
+Route::get('/mainpage', function () {
+    return view('main');
+}); 
